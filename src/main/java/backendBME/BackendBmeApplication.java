c348e7e291ffe5e7f0a532fd2ce6ca8cdbe71aab
singhnavneet188@gmail.com <navneet.singh071999@gmail.com>
2021-06-21 12:19:19 +0530

    first commit

---
+++ src/main/java/backendBME/BackendBmeApplication.java
@@ -0,0 +1,13 @@
+package backendBME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBmeApplication.class, args);
+	}
+
+}
